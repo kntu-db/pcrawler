fe60ec9256d62d375b9befa7cd84d9f682d79eed
Mahdi Lotfi <mahdi.vahshat.80@gmail.com>
2022-06-22 12:11:33 +0430

    Structure project

---
+++ README.md
@@ -0,0 +1,4 @@
+# PCrawler project
+A modular probleset crawler for crawl problems from varying websites.  
+Storing data in Cassandra database.  
+Including GUI utilities.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pcrawler',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Mahdi Lotfi',
+    author_email='mahdi.vahshat.80@gmail.com',
+    description='problem set crawler'
+)
